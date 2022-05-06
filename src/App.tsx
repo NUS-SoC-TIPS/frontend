b8d6eb94f9c75bd72370813a65775d6edc4a9259
Hanming Zhu <hanming@huawen.com>
2022-05-06 11:10:29 +0800

    chore: set up dev deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import { ReactElement } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): ReactElement<'div'> {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
