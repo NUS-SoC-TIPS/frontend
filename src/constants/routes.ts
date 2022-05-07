1169439a7bad579749760754710d89ff0a47f208
Hanming Zhu <hanming@huawen.com>
2022-05-07 11:29:49 +0800

    feat(constants): add route constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,9 @@
+// Unauthed routes
+export const ROOT = '/';
+
+// Authed routes
+export const INTERVIEWS = '/interviews';
+export const LEETCODE = '/leetcode';
+export const TASKS = '/tasks';
+export const SETTINGS = '/settings';
+export const ROOM = '/room';
