75f9ed7306c39a88e56c331c376fde10360eb048
Hanming Zhu <hanming@huawen.com>
2022-05-07 11:20:24 +0800

    refactor(routes): simplify landing page logic

--- src/routes/landing/Landing.tsx
+++ src/routes/landing/Landing.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import { Stack } from '@chakra-ui/react';
 
 import { Page } from 'components/page';
@@ -7,20 +7,12 @@ import { useAuth } from 'contexts/AuthContext';
 import { Hero } from './Hero';
 
 export const Landing = (): ReactElement => {
-  const [isLoggingIn, setIsLoggingIn] = useState(false);
-  const { login } = useAuth();
-
-  const handleGitHubLogin = async (): Promise<void> => {
-    setIsLoggingIn(true);
-    login().catch(() => {
-      setIsLoggingIn(false);
-    });
-  };
+  const { login, isLoggingIn } = useAuth();
 
   return (
     <Page>
       <Stack spacing={{ base: '8', lg: '6' }}>
-        <Hero isGettingStarted={isLoggingIn} onGetStarted={handleGitHubLogin} />
+        <Hero isGettingStarted={isLoggingIn} onGetStarted={login} />
       </Stack>
     </Page>
   );
