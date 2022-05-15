0f1c1c6c90f24062d3c8bb9c89f61f88dd505202
Hanming Zhu <hanming@huawen.com>
2022-05-15 20:09:10 +0800

    refactor(routes): build room page

---
+++ src/routes/room/RoomPage.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren, ReactElement } from 'react';
+import { Box, DarkMode } from '@chakra-ui/react';
+
+export const RoomPage = ({
+  children,
+}: PropsWithChildren<unknown>): ReactElement<
+  PropsWithChildren<unknown>,
+  typeof DarkMode
+> => {
+  return (
+    <DarkMode>
+      <Box
+        as="section"
+        display="flex"
+        flexDirection="column"
+        height="100vh"
+        width="100vw"
+      >
+        {children}
+      </Box>
+    </DarkMode>
+  );
+};
