4f1141bde837c4b1cde3278c0389c30f93ce5696
Hanming Zhu <hanming@huawen.com>
2022-05-22 21:30:48 +0800

    feat(components): update card styling

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -4,7 +4,8 @@ import { Box, BoxProps, useColorModeValue } from '@chakra-ui/react';
 const DEFAULT_PROPS: Partial<BoxProps> = {
   bg: 'bg-surface',
   borderRadius: 'lg',
-  minH: '36',
+  px: { base: '4', md: '6' },
+  py: { base: '5', md: '6' },
 };
 
 export const Card = (props: BoxProps): ReactElement<typeof Box> => (
