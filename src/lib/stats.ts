e20fbbe6d45249e1a202c58459e9b20ea4f9703a
Hanming Zhu <hanming@huawen.com>
2022-05-23 01:43:07 +0800

    feat(lib): add task stats route

--- src/lib/stats.ts
+++ src/lib/stats.ts
@@ -1,4 +1,4 @@
-import { QuestionStats } from 'types/api/stats';
+import { QuestionStats, TaskStats } from 'types/api/stats';
 import { api } from 'utils/apiUtils';
 
 export const getQuestionStats = async (): Promise<QuestionStats> => {
@@ -10,5 +10,33 @@ export const getQuestionStats = async (): Promise<QuestionStats> => {
       startAt: new Date(response.data.closestWindow.startAt),
       endAt: new Date(response.data.closestWindow.endAt),
     },
+  };
+};
+
+export const getTaskStats = async (): Promise<TaskStats> => {
+  const response = await api.get('stats/tasks');
+  return {
+    windows: (response.data as TaskStats).windows.map((w) => ({
+      ...w,
+      window: {
+        ...w.window,
+        startAt: new Date(w.window.startAt),
+        endAt: new Date(w.window.endAt),
+      },
+      submissions: w.submissions.map((s) => ({
+        ...s,
+        submission: {
+          ...s.submission,
+          createdAt: new Date(s.submission.createdAt),
+        },
+      })),
+      interviews: w.interviews.map((i) => ({
+        ...i,
+        record: {
+          ...i.record,
+          createdAt: new Date(i.record.createdAt),
+        },
+      })),
+    })),
   };
 };
