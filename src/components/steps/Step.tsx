36f11a7eb6963c0d2a0b4b9f7afde5f5acb6313e
Hanming Zhu <hanming@huawen.com>
2022-05-23 01:42:34 +0800

    feat(components): add steps

---
+++ src/components/steps/Step.tsx
@@ -0,0 +1,84 @@
+import { ReactElement } from 'react';
+import {
+  BoxProps,
+  Divider,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+
+import { StepCircle } from './StepCircle';
+
+interface Props extends BoxProps {
+  title: string;
+  description: string;
+  isSuccess: boolean;
+  isFailure: boolean;
+  isActive: boolean; // May not necessarily be success nor failure, just active
+  isFirstStep: boolean;
+  isLastStep: boolean;
+  leftLineColor?: string;
+  rightLineColor?: string;
+}
+
+export const Step = (props: Props): ReactElement<Props, typeof Stack> => {
+  const {
+    isSuccess,
+    isFailure,
+    isActive,
+    isFirstStep,
+    isLastStep,
+    leftLineColor,
+    rightLineColor,
+    title,
+    description,
+    ...stackProps
+  } = props;
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const orientation = useBreakpointValue<'horizontal' | 'vertical'>({
+    base: 'vertical',
+    md: 'horizontal',
+  });
+
+  return (
+    <Stack
+      direction={{ base: 'row', md: 'column' }}
+      flex="1"
+      spacing="4"
+      {...stackProps}
+    >
+      <Stack
+        align="center"
+        direction={{ base: 'column', md: 'row' }}
+        spacing="0"
+      >
+        <Divider
+          borderColor={isFirstStep ? 'transparent' : leftLineColor ?? 'inherit'}
+          borderWidth="1px"
+          orientation={orientation}
+        />
+        <StepCircle
+          isActive={isActive}
+          isFailure={isFailure}
+          isSuccess={isSuccess}
+        />
+        <Divider
+          borderColor={isLastStep ? 'transparent' : rightLineColor ?? 'inherit'}
+          borderWidth="1px"
+          orientation={orientation}
+        />
+      </Stack>
+      <Stack
+        align={{ base: 'start', md: 'center' }}
+        pb={isMobile && !isLastStep ? '8' : '0'}
+        spacing="0.5"
+      >
+        <Text color="emphasized" fontWeight="medium">
+          {title}
+        </Text>
+        <Text color="muted">{description}</Text>
+      </Stack>
+    </Stack>
+  );
+};
---
+++ src/components/steps/StepCircle.tsx
@@ -0,0 +1,32 @@
+import { ReactElement } from 'react';
+import { HiCheck, HiX } from 'react-icons/hi';
+import { Circle, Icon, SquareProps } from '@chakra-ui/react';
+
+interface Props extends SquareProps {
+  isSuccess: boolean;
+  isFailure: boolean;
+  isActive: boolean;
+}
+
+export const StepCircle = (
+  props: Props,
+): ReactElement<Props, typeof Circle> => {
+  const { isSuccess, isActive, isFailure } = props;
+  return (
+    <Circle
+      bg={isSuccess ? 'accent' : isFailure ? 'error' : 'inherit'}
+      borderColor={isActive ? 'accent' : 'inherit'}
+      borderWidth={isSuccess || isFailure ? '0' : '2px'}
+      size="8"
+      {...props}
+    >
+      {isSuccess ? (
+        <Icon as={HiCheck} boxSize="5" color="inverted" />
+      ) : isFailure ? (
+        <Icon as={HiX} boxSize="5" color="on-accent" />
+      ) : (
+        <Circle bg={isActive ? 'accent' : 'border'} size="3" />
+      )}
+    </Circle>
+  );
+};
