8bf47fa426ab1cb1e7cc532726b8d7009ba91260
Hanming Zhu <hanming@huawen.com>
2022-05-22 18:52:26 +0800

    feat(app): add questions pages to authenticated app

--- src/app/AuthenticatedApp.tsx
+++ src/app/AuthenticatedApp.tsx
@@ -1,16 +1,19 @@
 import { ReactElement } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { INTERVIEWS, ROOM } from 'constants/routes';
+import { ADD_QUESTION, INTERVIEWS, QUESTIONS, ROOM } from 'constants/routes';
 import { Interviews } from 'routes/interviews';
+import { AddQuestion, Questions } from 'routes/questions';
 import { Room } from 'routes/room';
 
 const AuthenticatedApp = (): ReactElement<typeof Routes> => {
   return (
     <Routes>
+      <Route element={<Questions />} path={QUESTIONS} />
+      <Route element={<AddQuestion />} path={ADD_QUESTION} />
       <Route element={<Interviews />} path={INTERVIEWS} />
       <Route element={<Room />} path={`${ROOM}/:slug`} />
-      <Route element={<Navigate to={INTERVIEWS} />} path="*" />
+      <Route element={<Navigate to={QUESTIONS} />} path="*" />
     </Routes>
   );
 };
