6387c5536aad609f13340a081f5be19b6fe4ceb9
Hanming Zhu <hanming@huawen.com>
2022-05-22 18:49:29 +0800

    feat(routes): add url form control for add questions

---
+++ src/routes/questions/form/UrlFormControl.tsx
@@ -0,0 +1,49 @@
+import { ReactElement } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Stack,
+} from '@chakra-ui/react';
+
+import { Question, QuestionSource } from 'types/models/question';
+
+interface Props {
+  question: Question;
+}
+
+export const UrlFormControl = ({
+  question,
+}: Props): ReactElement<Props, typeof FormControl> => {
+  const getUrl = (): string => {
+    switch (question.source) {
+      case QuestionSource.LEETCODE:
+        return `leetcode.com/problems/${question.slug}`;
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <FormControl id="url">
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        justify="space-between"
+        spacing={{ base: '1.5', md: '8' }}
+      >
+        <FormLabel variant="inline">URL</FormLabel>
+        <InputGroup maxW={{ md: '3xl' }}>
+          <InputLeftAddon>https://</InputLeftAddon>
+          <Input
+            _active={{}}
+            _focus={{}}
+            _hover={{ cursor: 'not-allowed' }}
+            value={getUrl()}
+          />
+        </InputGroup>
+      </Stack>
+    </FormControl>
+  );
+};
