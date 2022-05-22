00c0124bae70dbb5acffbd57ca5f43b9250fceb1
Hanming Zhu <hanming@huawen.com>
2022-05-22 18:47:34 +0800

    feat(lib): add questions api

---
+++ src/lib/questions.ts
@@ -0,0 +1,7 @@
+import { Question } from 'types/models/question';
+import { api } from 'utils/apiUtils';
+
+export const getQuestions = async (): Promise<Question[]> => {
+  const response = await api.get('questions');
+  return response.data;
+};
