5e71b9e9426648c469e718527309962223aa2890
Hanming Zhu <hanming@huawen.com>
2022-05-23 09:05:40 +0800

    feat(constants): add error toast props

--- src/constants/toast.ts
+++ src/constants/toast.ts
@@ -3,3 +3,10 @@ export const DEFAULT_TOAST_PROPS = {
   isClosable: true,
   position: 'bottom' as const,
 };
+
+export const ERROR_TOAST_PROPS = {
+  ...DEFAULT_TOAST_PROPS,
+  title: 'Uh oh!',
+  description: 'Something went wrong. Please try again.',
+  status: 'error' as const,
+};
