168323a85c314399d35dadfa26f72a671c0a1a79
Hanming Zhu <hanming@huawen.com>
2022-05-29 15:30:38 +0800

    feat(types): add role to user

--- src/types/models/user.ts
+++ src/types/models/user.ts
@@ -6,6 +6,12 @@ export interface User {
   githubUsername: string;
   photoUrl: string;
   profileUrl: string;
+  role: UserRole;
+}
+
+export enum UserRole {
+  NORMAL = 'NORMAL',
+  ADMIN = 'ADMIN',
 }
 
 export interface Settings {
