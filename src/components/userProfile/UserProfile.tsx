e94148a5013bb7d859d08b80096c0a8aaf8a1292
Hanming Zhu <hanming@huawen.com>
2022-05-30 08:33:36 +0800

    feat(components): make user profile name a link to the github profile

--- src/components/userProfile/UserProfile.tsx
+++ src/components/userProfile/UserProfile.tsx
@@ -1,5 +1,5 @@
 import { memo, ReactElement } from 'react';
-import { Avatar, Box, HStack, Text } from '@chakra-ui/react';
+import { Avatar, Box, HStack, Link, Text } from '@chakra-ui/react';
 
 import { User } from 'types/models/user';
 
@@ -14,9 +14,15 @@ const RawUserProfile = ({
     <HStack ps={2} spacing={3}>
       <Avatar boxSize={10} name={user.name} src={user.photoUrl} />
       <Box>
-        <Text fontSize="sm" fontWeight="medium" textAlign="left">
+        <Link
+          fontSize="sm"
+          fontWeight="medium"
+          href={user.profileUrl}
+          isExternal={true}
+          textAlign="left"
+        >
           {user.name}
-        </Text>
+        </Link>
         <Text color="muted" fontSize="sm" textAlign="left">
           {user.githubUsername}
         </Text>
