508705c48e25224a4aceae64c9ac26069c463a53
Hanming Zhu <hanming@huawen.com>
2022-06-12 18:33:46 +0800

    feat: add unauthenticated room

--- src/app/UnauthenticatedApp.tsx
+++ src/app/UnauthenticatedApp.tsx
@@ -1,13 +1,15 @@
 import { ReactElement } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { ROOT } from 'constants/routes';
+import { ROOM, ROOT } from 'constants/routes';
 import { Landing } from 'routes/landing';
+import { UnauthenticatedRoom } from 'routes/room';
 
 const UnauthenticatedApp = (): ReactElement<typeof Routes> => {
   return (
     <Routes>
       <Route element={<Landing />} path={ROOT} />
+      <Route element={<UnauthenticatedRoom />} path={`${ROOM}/:slug`} />
       <Route element={<Navigate to={ROOT} />} path="*" />
     </Routes>
   );
---
+++ src/routes/room/UnauthenticatedRoom.tsx
@@ -0,0 +1,49 @@
+import { ReactElement } from 'react';
+import { Button, Text, useToast } from '@chakra-ui/react';
+
+import { GitHubIcon } from 'components/icons/GitHubIcon';
+import { Modal } from 'components/modal';
+import { ERROR_TOAST_PROPS } from 'constants/toast';
+import { useAuth } from 'contexts/AuthContext';
+
+import { RoomPage } from './RoomPage';
+
+export const UnauthenticatedRoom = (): ReactElement<typeof RoomPage> => {
+  const { login, isLoggingIn } = useAuth();
+  const toast = useToast();
+
+  const onLogin = async (): Promise<void> => {
+    login().catch(() => toast(ERROR_TOAST_PROPS));
+  };
+
+  return (
+    <RoomPage>
+      <Modal
+        isOpen={true}
+        onClose={(): void => undefined}
+        showActions={false}
+        textAlignCentre={true}
+        title="Login now to get started with your mock interviews!"
+      >
+        <Text color="muted">
+          No worries, it&apos;s quick and painless to get started with TIPS!
+        </Text>
+        <Button
+          fontSize="md"
+          fontWeight="bold"
+          iconSpacing={3}
+          isLoading={isLoggingIn}
+          leftIcon={<GitHubIcon boxSize={5} />}
+          mt={4}
+          onClick={onLogin}
+          px={8}
+          size="lg"
+          variant="primary"
+          width="full"
+        >
+          Get started with GitHub
+        </Button>
+      </Modal>
+    </RoomPage>
+  );
+};
