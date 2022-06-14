0a24affacc014350eeb154968e0054209ec301d8
Hanming Zhu <hanming@huawen.com>
2022-06-14 10:19:18 +0800

    docs(types): improve table props documentation

--- src/types/table/index.ts
+++ src/types/table/index.ts
@@ -2,35 +2,46 @@
 import { ReactNode } from 'react';
 
 export interface TableColumnOptions {
+  /** Render a custom string or ReactNode using the cell value. */
   customBodyRenderer?: (value: any) => string | ReactNode;
-  /** To render a string, just use the label property on TableColumn */
+  /** To render a string, use the label property on TableColumn instead. */
   customHeaderRenderer?: () => ReactNode;
-  customCsvBodyRenderer?: (value: any) => string;
-  customCsvHeaderRenderer?: () => string;
-  /** Default is true */
+  /** Default is true. */
   isVisible?: boolean;
-  /** Default is true */
+  /** Whether the column will be exported to CSV. Default is true. Column can be downloadable even if not visible. */
   isDownloadable?: boolean;
-  /** Default is true */
+  /** Render a custom string to export to CSV using the cell value. */
+  customCsvBodyRenderer?: (value: any) => string;
+  /** Render a custom column header string to export to CSV. If not provided, the column label is used. */
+  customCsvHeaderRenderer?: () => string;
+  /** Default is true. Column can be searchable even if not visible. */
   isSearchable?: boolean;
+  /** Render a custom string representation for searching purposes. */
   customSearchValueRenderer?: (value: any) => string;
-  /** Default is false */
+  /** Default is false. Column is not sortable if it is not visible. */
   isSortable?: boolean;
+  /** If not provided, numbers will be compared as numbers and everything else will be compared as strings. */
   customSortComparator?: (a: any, b: any) => number;
 }
 
 export interface TableColumn {
+  /** Label to render as the column header for both the table and the CSV download. */
   label: string;
+  /** Key that is to be used to index against each row object to obtain the cell data. */
   key: string;
+  /** See TableColumnOptions. */
   options?: TableColumnOptions;
 }
 
 export interface TableOptions {
+  /** Default is `Table`. */
   title?: string;
+  /** Message to show when no data is available. Default is `No Data`. */
   noDataMessage?: string;
-  /** Default is true */
+  /** Whether the table supports export to CSV. Default is true. */
   isDownloadable?: boolean;
-  /** Default is true */
+  /** Whether the table supports searching. Default is true. */
   isSearchable?: boolean;
+  /** Downloaded CSV file name. Default is `Table`. No need to provide the `.csv` suffix. */
   downloadFileName?: string;
 }
