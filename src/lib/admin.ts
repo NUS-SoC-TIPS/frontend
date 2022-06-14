09936a89ae7638abf389442faeaa57c6d7e6bdb9
Hanming Zhu <hanming@huawen.com>
2022-06-14 09:41:28 +0800

    feat(lib): add api calls for exclusions

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,4 +1,5 @@
-import { AdminStatsEntity } from 'types/api/admin';
+import { AdminStatsEntity, CreateExclusionDto } from 'types/api/admin';
+import { Exclusion } from 'types/models/exclusion';
 import { Window } from 'types/models/window';
 import { api } from 'utils/apiUtils';
 
@@ -11,5 +12,17 @@ export const getAdminStats = async (
   windowId: number,
 ): Promise<AdminStatsEntity> => {
   const response = await api.get(`admin/stats/${windowId}`);
+  return response.data;
+};
+
+export const createExclusion = async (
+  dto: CreateExclusionDto,
+): Promise<Exclusion> => {
+  const response = await api.post('admin/exclusions', dto);
+  return response.data;
+};
+
+export const deleteExclusion = async (id: number): Promise<void> => {
+  const response = await api.delete(`admin/exclusions/${id}`);
   return response.data;
 };
