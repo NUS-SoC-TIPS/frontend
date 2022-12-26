456bb93313712061a4dc172678528872fc0f2848
Hanming Zhu <hanming@huawen.com>
2022-12-26 09:29:53 -0800

    feat(routes): add code execution error handling

--- src/routes/room/bottomBar/CodeExecutionButton.tsx
+++ src/routes/room/bottomBar/CodeExecutionButton.tsx
@@ -1,9 +1,11 @@
-import { ReactElement } from 'react';
-import { Button, Tooltip } from '@chakra-ui/react';
+import { ReactElement, useEffect } from 'react';
+import { Button, Tooltip, useToast } from '@chakra-ui/react';
 import { Socket } from 'socket.io-client';
 
 import { useAppSelector } from 'app/hooks';
+import { ERROR_TOAST_PROPS } from 'constants/toast';
 import { executeCode } from 'lib/codeSocket';
+import { CodeExecutionError } from 'reducers/codeReducer';
 
 interface Props {
   socket: Socket;
@@ -12,9 +14,29 @@ interface Props {
 export const CodeExecutionButton = ({
   socket,
 }: Props): ReactElement<Props, typeof Button> => {
-  const { language, executableLanguageToVersionMap, isExecuting } =
-    useAppSelector((state) => state.code);
+  const {
+    language,
+    executableLanguageToVersionMap,
+    isExecuting,
+    executionError,
+  } = useAppSelector((state) => state.code);
   const versionName = executableLanguageToVersionMap[language];
+  const toast = useToast();
+
+  useEffect(() => {
+    if (executionError === CodeExecutionError.FAILED_TO_START_EXECUTION) {
+      toast({
+        ...ERROR_TOAST_PROPS,
+        title: 'Failed to start execution!',
+      });
+    } else if (executionError === CodeExecutionError.EXECUTION_TIMED_OUT) {
+      toast({
+        ...ERROR_TOAST_PROPS,
+        title: 'Execution timed out!',
+      });
+    }
+  }, [executionError, toast]);
+
   if (versionName == null) {
     return (
       <Tooltip label="Execution is not supported for this language">
