dc5f25b24c2f02463868f05d631b6549414986d7
Hanming Zhu <hanming@huawen.com>
2023-05-26 08:47:10 -0700

    feat: support showing coursemology link

--- src/routes/tasks/breakdown/TasksBreakdownPage.tsx
+++ src/routes/tasks/breakdown/TasksBreakdownPage.tsx
@@ -1,14 +1,20 @@
 import { PropsWithChildren, ReactElement } from 'react';
+import { FiExternalLink } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@chakra-ui/react';
+import { Button, Link, Stack } from '@chakra-ui/react';
 
 import { Dashboard, Page } from 'components/page';
 import { TASKS } from 'constants/routes';
 
+interface Props {
+  coursemologyUrl?: string;
+}
+
 export const TasksBreakdownPage = ({
+  coursemologyUrl,
   children,
-}: PropsWithChildren<unknown>): ReactElement<
-  PropsWithChildren<unknown>,
+}: PropsWithChildren<Props>): ReactElement<
+  PropsWithChildren<Props>,
   typeof Page
 > => {
   const navigate = useNavigate();
@@ -16,9 +22,29 @@ export const TasksBreakdownPage = ({
     <Page>
       <Dashboard
         actions={
-          <Button onClick={(): void => navigate(TASKS)} variant="primary">
-            Back to Cohorts
-          </Button>
+          <Stack direction="row" spacing={3}>
+            {coursemologyUrl ? (
+              <Link href={coursemologyUrl} isExternal={true}>
+                <Button
+                  leftIcon={<FiExternalLink fontSize="1.25rem" />}
+                  variant="secondary"
+                >
+                  Coursemology
+                </Button>
+              </Link>
+            ) : (
+              <Button
+                isDisabled={true}
+                leftIcon={<FiExternalLink fontSize="1.25rem" />}
+                variant="secondary"
+              >
+                Coursemology
+              </Button>
+            )}
+            <Button onClick={(): void => navigate(TASKS)} variant="primary">
+              Back to Cohorts
+            </Button>
+          </Stack>
         }
         heading="Tasks"
         subheading="Track your progress for this cohort here!"
